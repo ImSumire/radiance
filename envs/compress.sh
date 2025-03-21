82c0635e24269c2ff5cd524b0f886be4fe08f232
ImSumire <violette.fenneteau@gmail.com>
2025-03-21 22:54:39 +0100

    Fixed compress.sh (I think)

--- envs/compress.sh
+++ envs/compress.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-for item in envs/src/*; do
+cd envs
+
+for item in src/*; do
     if [ -d "$item" ]; then  # Check if the item is a directory
         dir=$(basename "$item")
-        tar -cJf "envs/$dir.tar.xz" "$item"
+        tar -cJf "$dir.tar.xz" "$item"
         echo "Compressed $dir to envs/$dir.tar.xz"
     fi
 done
