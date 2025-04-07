221abef274c6e93523b9a7a8b224902ac46faee3
ImSumire <violette.fenneteau@gmail.com>
2025-04-07 15:50:28 +0200

    Fixing async message by removing "Done.\n"

--- envs/src/raylibc/scripts/common/dlib.sh
+++ envs/src/raylibc/scripts/common/dlib.sh
@@ -2,10 +2,9 @@
 
 DLIB_BANNER="\e[34;1mdlib\e[0m"
 
-printf "$DLIB_BANNER Compiling libhot.so..."
+printf "$DLIB_BANNER Compiling libhot.so\n"
 cc -Og -g \
    -o target/hot/libhot.so \
    radiance/extern.c \
    target/hot/libraylib.so.??? \
    -fPIC -shared -lm
-printf " Done.\n"
--- envs/src/raylibcpp/scripts/common/dlib.sh
+++ envs/src/raylibcpp/scripts/common/dlib.sh
@@ -2,10 +2,9 @@
 
 DLIB_BANNER="\e[34;1mdlib\e[0m"
 
-printf "$DLIB_BANNER Compiling libhot.so..."
+printf "$DLIB_BANNER Compiling libhot.so\n"
 g++ -Og -g \
     -o target/hot/libhot.so \
     radiance/extern.cpp \
     target/hot/libraylib.so.??? \
     -fPIC -shared -lm
-printf " Done.\n"
